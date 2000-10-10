7ac2e1cdc99cec17afd995c3eb846c96ca06893c
Scott McPeak <smcpeak@cs.berkeley.edu>
2000-10-10 02:39:11 +0000

    snap

---
+++ elkhound/cc_env.h
@@ -0,0 +1,18 @@
+// cc_env.h
+// Env class, which is the compile-time C++ environment
+
+#ifndef __CC_ENV_H
+#define __CC_ENV_H
+
+class Type;
+class AtomicType;
+
+class Environment {
+
+
+
+
+
+
+
+#endif // __CC_ENV_H
