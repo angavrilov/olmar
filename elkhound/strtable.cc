f7d6ba01a2a9905beef930620473deeaab3cccad
Scott McPeak <smcpeak@cs.berkeley.edu>
2000-10-18 08:49:54 +0000

    implemented a string table

---
+++ elkhound/strtable.cc
@@ -0,0 +1,67 @@
+// strtable.cc
+// code for strtable.h
+
+#include "strtable.h"    // this module
+#include "xassert.h"     // xassert
+                          
+
+STATICDEF char const *StringTable::identity(void *data)
+{
+  return (char const*)data;
+}
+
+
+StringTable::StringTable()
+  : hash(identity)
+{
+  firstRack = new Rack(NULL);
+  lastRack = firstRack;
+}
+
+
+StringTable::~StringTable()
+{
+  while (firstRack != NULL) {
+    Rack *temp = firstRack;
+    firstRack = firstRack->next;
+    delete temp;
+  }
+}
+
+
+StringRef StringTable::add(char const *src)
+{
+  // see if it's already here
+  StringRef ret = get(src);
+  if (ret) {
+    return NULL;
+  }
+
+  // see if we need a new rack
+  int len = strlen(src)+1;     // include nul terminator
+  if (len > lastRack->availBytes()) {
+    // need a new rack
+    xassert(len <= rackSize);
+    lastRack = new Rack(lastRack);
+    
+    // note that if we need longer strings, we can accomodate
+    // them by allocating bigger racks as a special case (and
+    // just casting around the type system)
+  }
+  
+  // enter the intended location into the indexing structures
+  ret = lastRack->nextByte();
+  hash.add(ret, (void*)ret);
+
+  // add the string to the last rack
+  memcpy(lastRack->nextByte(), src, len);
+  lastRack->usedBytes += len;
+
+  return ret;
+}
+
+
+StringRef StringTable::get(char const *src) const
+{
+  return (StringRef)hash.get(src);
+}
---
+++ elkhound/strtable.h
@@ -0,0 +1,70 @@
+// strtable.h
+// implements a collection of immutable strings (assumed relatively
+// short length) with unique representatives
+
+#ifndef STRTABLE_H
+#define STRTABLE_H
+
+#include "strhash.h"     // StringHash
+
+
+// the type of references to strings in a string table; the pointer
+// can be used directly in equality comparisons, because several calls
+// to 'add' return the same pointer; and it points to the represented
+// string (null-terminated), so it can be printed directly, etc.
+typedef char const *StringRef;
+
+
+class StringTable {
+private:    // types                                       
+  // constants
+  enum {                 
+    rackSize = 16000,      // size of one rack; also max string length
+  };
+
+  // some of the strings stored in the table
+  struct Rack {
+    Rack *next;            // (owner) next rack, if any; for deallocation
+    int usedBytes;         // # of bytes of 'data' that are used
+    char data[rackSize];   // data where strings are stored
+
+  public:
+    Rack(Rack *n) : next(n), usedBytes(0) {}
+    int availBytes() const { return rackSize - usedBytes; }
+    char *nextByte() { return data + usedBytes; }
+  };
+
+private:    // data
+  // hash table mapping strings to pointers into one
+  // of the string racks
+  StringHash hash;
+
+  // linked list of racks; only walked at dealloc time
+  Rack *firstRack;
+  Rack *lastRack;          // invariant: lastRack->next == NULL
+
+private:    // funcs
+  // not allowed
+  StringTable(StringTable&);
+  void operator=(StringTable&);
+  void operator==(StringTable&);
+
+  // for mapping data to keys, in the hashtable
+  static char const *identity(void *data);
+
+public:     // funcs
+  StringTable();
+  ~StringTable();
+
+  // add 'src' to the table, if it isn't already there; return a
+  // unique representative, such that multiple calls to 'add' with
+  // the same string contents will always yield the same value
+  StringRef add(char const *src);
+
+  // if 'src' is in the table, return its representative; if not,
+  // return NULL
+  StringRef get(char const *src) const;
+};
+
+
+#endif // STRTABLE_H
