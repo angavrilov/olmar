ceb3ac4cd7465fce39ffc64e01cbb5415ababce7
Scott McPeak <smcpeak@cs.berkeley.edu>
2000-12-13 20:33:37 +0000

    added some things for alloc tracking

--- smbase/macros.h
+++ smbase/macros.h
@@ -142,4 +142,30 @@ inline void pretendUsedFn(T const &) {}
   default: break; /*silence warning*/ 
 
 
+// for a class that maintains allocated-node stats
+#define ALLOC_STATS_DECLARE                     \
+  static int numAllocd;                         \
+  static int maxAllocd;                         \
+  static void printAllocStats(bool anyway);
+
+// these would go in a .cc file, whereas above goes in .h file
+#define ALLOC_STATS_DEFINE(classname)                      \
+  int classname::numAllocd = 0;                            \
+  int classname::maxAllocd = 0;                            \
+  STATICDEF void classname::printAllocStats(bool anyway)   \
+  {                                                        \
+    if (anyway || numAllocd != 0) {                        \
+      cout << #classname << " nodes: " << numAllocd        \
+           << ", max  nodes: " << maxAllocd                \
+           << endl;                                        \
+    }                                                      \
+  }
+
+#define ALLOC_STATS_IN_CTOR                     \
+  INC_HIGH_WATER(numAllocd, maxAllocd);
+
+#define ALLOC_STATS_IN_DTOR                     \
+  numAllocd--;
+
+
 #endif // __MACROS_H
