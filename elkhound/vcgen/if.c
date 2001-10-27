89d3791ce18b84d27d579168269c582f06c8cb7a
Scott McPeak <smcpeak@cs.berkeley.edu>
2001-10-27 14:22:43 +0000

    verified 2 if's one-after-another work

--- elkhound/vcgen/if.c
+++ elkhound/vcgen/if.c
@@ -8,9 +8,34 @@ void foo()
     y = 2;
   }
   else {
+    thmprv_assert x == 0;
     y = 3;
   }
-  
+
   thmprv_assert y >= 2;
 }
+
+
+void bar(int x, int y)
+{   
+  int z = 0;
+
+  if (x) {
+    z = z + 1;
+  }     
+  else {
+    z = z + 2;
+  }
+  
+  if (y) {
+    z = z + 10;
+  }
+  else {
+    z = z + 20;
+  }
+  
+  thmprv_assert z==11 || z==12 ||
+                z==21 || z==22;
+}
+
 
