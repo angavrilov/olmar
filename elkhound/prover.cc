2f1459575854f957c1d47d2afe8cedcc774902d8
Scott McPeak <smcpeak@cs.berkeley.edu>
2001-11-26 10:22:05 +0000

    optimized method for calling Simplify

--- elkhound/prover.cc
+++ elkhound/prover.cc
@@ -1,46 +1,79 @@
 // prover.cc
 // code to run Simplify
                                    
-// needed for mkstemp
-#define _XOPEN_SOURCE
-#define _XOPEN_SOURCE_EXTENDED
-
 #include "prover.h"    // this module
 #include "str.h"       // stringc
+#include "mypopen.h"   // popen_execvp
+#include "unixutil.h"  // readString, writeAll
 
-#include <stdlib.h>    // mkstemp
-#include <unistd.h>    // write
-#include <stdio.h>     // perror
+#include <stdio.h>     // perror, FILE stuff
+#include <string.h>    // strstr
 
 bool runProver(char const *str)
 {
-  // make a temp file
-  char fname[] = "/tmp/proverXXXXXX";
-  int fd = mkstemp(fname);
-  if (fd < 0) {
-    perror("mkstemp");
+  static bool opened = false;             // true once we've opened the process
+  static int toSimplify, fromSimplify;    // pipe file descriptors
+
+  if (!opened) {
+    // open Simplify as a child process, with pipes to communicate
+    char *argv[] = { "Simplify", "-nosc", NULL };
+    popen_execvp(&toSimplify, &fromSimplify, NULL, argv[0], argv);
+
+    // open background predicate
+    FILE *bgpred = fopen("bgpred.sx", "r");
+    if (!bgpred) {
+      perror("open bgpred.sx");
+      exit(2);
+    }
+
+    // read the background predicate and feed it to Simplify
+    enum { BUFSIZE = 4096 };
+    char buf[BUFSIZE];
+    int len;
+    while ((len = fread(buf, 1, BUFSIZE, bgpred)) > 0) {
+      if (!writeAll(toSimplify, buf, len)) {
+        perror("write bgpred to Simplify");
+        exit(2);
+      }
+    }
+    fclose(bgpred);
+
+    // read Simplify's response
+    if (!readString(fromSimplify, buf, BUFSIZE)) {
+      perror("read from Simplify");
+      exit(2);
+    }
+    if (0!=strcmp(buf, ">\t")) {
+      fprintf(stderr, "unexpected response from Simplify after bgpred:\n");
+      fprintf(stderr, "%s\n", buf);
+      exit(2);
+    }
+
+    opened = true;
+  }
+
+  // hand the current predicate to Simplify
+  if (!writeAll(toSimplify, str, strlen(str))) {
+    perror("write to Simplify");
     exit(2);
   }
 
-  // stash the contents of 'str' in it
-  write(fd, str, strlen(str));
-  write(fd, "\n", 1);    // necessary?
-
-  if (close(fd) < 0) {
-    perror("close");
+  // read Simplify's response
+  char response[80];
+  if (!readString(fromSimplify, response, 80)) {
+    perror("read from Simplify");
     exit(2);
   }
 
-
-  // run Simplify on that file
-  int code = system(stringc << "cat bgpred.sx " << fname << " | Simplify | grep Valid >/dev/null");
-  
-  
-  // clean up the temp file
-  if (unlink(fname) < 0) {
-    perror("unlink");      // but continue anyway
+  if (strstr(response, "Invalid")) {
+    return false;          // formula is not proven
   }
-  
-  
-  return code == 0;
+  if (strstr(response, "Valid")) {
+    return true;           // formula is proven
+  }
+
+  fprintf(stderr, "unexpected response from Simplify:\n");
+  fprintf(stderr, "%s\n", response);
+  exit(2);
+  return false;            // not reached; silence warning
 }
---
+++ elkhound/vcgen/global2.c
@@ -0,0 +1,66 @@
+// global.c
+// experimenting with global variables
+
+int x thmprv_attr(addrtaken);
+int y thmprv_attr(addrtaken);
+
+void addone()
+  thmprv_pre
+    int pre_x = x; true;
+  thmprv_post
+    x == pre_x + 1;
+{
+  x = x + 1;
+}
+
+void bar()
+  thmprv_pre
+    int pre_x = x;
+    x > 0;
+  thmprv_post
+    x == pre_x;
+{}
+
+int *object(int *ptr);
+int offset(int *ptr);
+int length(int *obj);
+int select(int *mem, int *obj, int offset);
+int update(int *mem, int *obj, int offset, int value);
+
+void inc(int *x)
+  thmprv_pre 
+    int *pre_mem = mem;
+    int pre_y = y;
+    //object(x) != object(&y) &&
+    x != &y &&
+    offset(x) >= 0 && offset(x) < length(object(x));
+  thmprv_post 
+    mem == update(pre_mem, object(x), offset(x),
+                  select(pre_mem, object(x), offset(x))+1) &&
+    y == pre_y;
+{
+  *x = *x + 1;
+}
+
+
+int main()
+{
+  x = 2;
+  thmprv_assert x == 2;
+
+  addone();
+  thmprv_assert x == 3;
+
+  bar();
+  thmprv_assert x == 3;
+
+  y = 6;
+  thmprv_assert y == 6;
+
+  inc(&x);
+  thmprv_assert x == 4;
+  thmprv_assert y == 6;
+
+  return 0;
+}
+
