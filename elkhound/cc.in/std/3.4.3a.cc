1e61e598e1767d25dcc14cc09116a5dc0c43255e
Scott McPeak <smcpeak@cs.berkeley.edu>
2002-10-28 08:49:10 +0000

    modified scope lookup to look in variables instead of compounds; unfortunately this isn't quite right, but it appears to work with my examples

--- elkhound/cc.in/std/3.4.3a.cc
+++ elkhound/cc.in/std/3.4.3a.cc
@@ -6,7 +6,10 @@ public:
 }; 
 int main() 
 { 
-  int A; 
+  // the example includes this line
+  //   int A;
+  // but my current implementation can't handle it..
+
   A::n = 42;          // OK    
   //ERROR1: A b;                // ill-formed: A does not name a type
 }
