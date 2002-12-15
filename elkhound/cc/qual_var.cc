fa4fea2292d90bbcbf792d69c87a945f68e5c229
Scott McPeak <smcpeak@cs.berkeley.edu>
2002-12-15 13:59:59 +0000

    committing these files now; they compile, but I haven't linked them into anything else yet

---
+++ elkhound/cc/qual_var.cc
@@ -0,0 +1,51 @@
+// qual_var.cc
+// code for qual_var.h
+
+#include "qual_var.h"      // this module
+#include "variable.h"      // Variable
+#include "qual_type.h"     // Type, Type_Q
+
+#include <iostream.h>      // cout
+
+Variable_Q::Variable_Q(Variable *v, Type_Q *t)
+  : var(v), qtype(t)
+{
+  instances.prepend(this);
+}
+
+Variable_Q::~Variable_Q()
+{
+  // linear time removal so quadratic time removal of all elements..
+  // since for now we never delete Variable_Qs it won't matter, but
+  // if at some point we do then a doubly-linked list should be used
+  // instead of SObjList
+
+  static bool first = true;
+  if (first) {
+    // so we notice if these things start getting deleted
+    cout << "hey!  there's a performance problem in " << __FILE__ << endl;
+    first = false;
+  }
+
+  instances.removeAt(instances.indexOf(this));
+}
+
+
+Variable_Q *Variable_Q::deepClone() const
+{
+  return new Variable_Q(var, qtype->deepClone());
+}
+
+
+void Variable_Q::checkHomomorphism() const
+{
+  // annotation relation is 1-1
+  xassert(var->q == this);
+  
+  // our notion of the variable's type should correspond with
+  // the C++ tcheck's notion of its type
+  xassert(qtype->type() == var->type);
+
+  // and that type should be self-consistent
+  qtype->checkHomomorphism();
+}
