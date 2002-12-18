a85cbf2f53358d4cf7e2e280590a45f26ccb12a3
Daniel Wilkerson <daniel.wilkerson@gmail.com>
2002-12-18 21:10:39 +0000

    separating cc_qual from cc and cc works but cc_qual not yet; more to come, don't use this version

--- elkhound/cc/dswutil.h
+++
@@ -1,9 +0,0 @@
-#ifndef DSWUTIL_H
-#define DSWUTIL_H
-
-#include "cc.ast.gen.h"
-
-PQName *appendToDeclaratorName(Declarator *d, StringRef suffix);
-PQName *appendToDeclaratorName(IDeclarator *d, StringRef suffix);
-
-#endif // DSWUTIL_H
