49e59e86fe2b90661ac582b2449a7222001428b2
Scott McPeak <smcpeak@cs.berkeley.edu>
2003-01-16 05:25:52 +0000

    added some macros I need for elsa

--- smbase/macros.h
+++ smbase/macros.h
@@ -207,4 +207,28 @@ public:
 };
 
 
+// declare a bunch of a set-like operators for enum types
+#define ENUM_BITWISE_AND(Type)                  \
+  inline Type operator& (Type f1, Type f2)      \
+    { return (Type)((int)f1 & (int)f2); }       \
+  inline Type& operator&= (Type &f1, Type f2)   \
+    { return f1 = f1 & f2; }
+
+#define ENUM_BITWISE_OR(Type)                   \
+  inline Type operator| (Type f1, Type f2)      \
+    { return (Type)((int)f1 | (int)f2); }       \
+  inline Type& operator|= (Type &f1, Type f2)   \
+    { return f1 = f1 | f2; }
+
+#define ENUM_BITWISE_NOT(Type, ALL)             \
+  inline Type operator~ (Type f)                \
+    { return (Type)((~(int)f) & ALL); }
+
+#define ENUM_BITWISE_OPS(Type, ALL)             \
+  ENUM_BITWISE_AND(Type)                        \
+  ENUM_BITWISE_OR(Type)                         \
+  ENUM_BITWISE_NOT(Type, ALL)
+
+
+
 #endif // __MACROS_H
