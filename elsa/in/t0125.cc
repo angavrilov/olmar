b36b37cf793423e47312eeac5c3747a738f37a27
Daniel Wilkerson <daniel.wilkerson@gmail.com>
2003-03-10 20:34:30 +0000

    The op constraints following some inline assembly can have two colons in a row if some constraints of one kind are not needed.  Some people (Linus?) sometimes write these two colons immediately adjacent, which in C++ is a different token.  We now allow allow this to parse, which is necessary for allowing the Linux kernel to parse.

--- elsa/in/t0125.cc
+++ elsa/in/t0125.cc
@@ -51,3 +51,22 @@ extern inline void read_lock(rwlock_t *rw)
          ) ;
   } while (0) ;
 }
+
+// this is another copy but retaining the double colon, which is a
+// single token in C++
+extern inline void read_lock2(rwlock_t *rw)
+{
+	do {
+          if (true)//__builtin_constant_p( rw ))
+            asm volatile("lock ; "  "subl $1,%0\n\t" "js 2f\n" "1:\n" ".section .text.lock,\"ax\"\n" "2:\tpushl %%eax\n\t" "leal %0,%%eax\n\t" "call "     "__read_lock_failed"   "\n\t" "popl %%eax\n\t" "jmp 1b\n" ".previous" :"=m" ((*(__dummy_lock_t *)(   rw   )) )) ; else asm volatile("lock ; "  "subl $1,(%0)\n\t" "js 2f\n" "1:\n" ".section .text.lock,\"ax\"\n" "2:\tcall "     "__read_lock_failed"   "\n\t" "jmp 1b\n" ".previous"
+  ::"a" (  rw  )                // NOTE double colon!
+  : "memory") ; 
+        } while (0) ;
+}
+
+void oink() {
+  // three-colons now works also!
+  asm ("asdfasd" ::: "a"(rw) );
+  // and four
+  asm ("asdfasd" :::: "a"(rw) );
+}
