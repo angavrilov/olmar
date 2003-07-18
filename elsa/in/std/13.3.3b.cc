d3e1ee7319398eedf65a269416bd504a37cd3185
Scott McPeak <smcpeak@cs.berkeley.edu>
2003-07-18 04:15:28 +0000

    implemented 12.3.2 para 4: ConversionTypeId parsing fix

--- elsa/in/std/13.3.3b.cc
+++ elsa/in/std/13.3.3b.cc
@@ -6,7 +6,10 @@ void Fcn(int*, int);                 // line 4
 int i;
 short s = 0;
 
-void f() {
+void f() {                           // line 9
+  // turn on overload resolution
+  __testOverload(f(), 9);
+
   //ERROR(1): Fcn(&i, s);            // is ambiguous because
                                      // &i->int* is better than &i->const int*
                                      // but s->short is also better than s->int
