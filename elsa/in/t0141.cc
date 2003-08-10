d4884f1634627818548206679f047ddd83ea1f7d
Scott McPeak <smcpeak@cs.berkeley.edu>
2003-08-10 23:54:02 +0000

    a couple more tests

--- elsa/in/t0141.cc
+++ elsa/in/t0141.cc
@@ -20,5 +20,54 @@ void f1()
 }
 
 
+struct A {
+  void operator~ ();     // line 24
+};
+  
+struct B {
+  operator int ();       // line 28
+};
+
+void f2()
+{
+  A a;
+  B b;
+  
+  __testOverload(~a, 24);
+  __testOverload(~b, 0);
+}
+
+
+
+struct C {
+  void operator~ ();     // line 43
+  operator char ();      // line 44
+};
+
+struct D {
+  void operator~ ();     // line 48
+  operator int ();       // line 49
+};
+
+void f3()
+{
+  C c;
+  D d;
+                         
+  // operator~ wins, because the conversion to char and then
+  // to int requires a promotion
+  __testOverload(~c, 43);       
+  
+  // operator~ still wins; operator int() is considered a conversion,
+  // whereas use of operator~ doesn't require any conversions
+  __testOverload(~d, 48);
+}
+
+
+
+
+
+
+
 
 
