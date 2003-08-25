20fd8c09ee6c6fdc6681464340bef2f2e8eaf30d
Scott McPeak <smcpeak@cs.berkeley.edu>
2003-08-25 22:44:58 +0000

    fixed a bug in VoidTailList::selfCheck, and added test code for vdtllist

--- smbase/vdtllist.cc
+++ smbase/vdtllist.cc
@@ -139,13 +139,52 @@ void VoidTailList::selfCheck() const
   VoidList::selfCheck();
 
   if (isNotEmpty()) {
-    // verify 'tail' is in list
-    xassert(contains(tail));
+    // get last node
+    VoidNode *n = top;
+    while (n->next) {
+      n = n->next;
+    }
 
-    // verify 'tail' is last element
-    xassert(tail->next == NULL);
+    // 'tail' should be the last one
+    xassert(tail == n);
   }
   else {
     xassert(tail == NULL);
   }
 }
+
+
+// --------------------- test code ------------------
+#ifdef TEST_VDTLLIST           
+
+#include <stdio.h>    // printf
+
+int main()
+{
+  VoidTailList list;
+  int zero, one, two, three;
+
+  // This isn't a very exhaustive test; it's mainly to check that
+  // selfCheck doesn't do anything really stupid (it used to).
+
+  list.selfCheck();
+
+  list.append(&two);     list.selfCheck();
+  list.prepend(&one);    list.selfCheck();
+  list.append(&three);   list.selfCheck();
+  list.prepend(&zero);   list.selfCheck();
+
+  xassert(list.nth(0) == &zero);
+  xassert(list.nth(1) == &one);
+  xassert(list.nth(2) == &two);
+  xassert(list.nth(3) == &three);
+
+  list.removeAll();
+  list.selfCheck();
+
+  printf("vdtllist works\n");
+
+  return 0;
+}
+
+#endif // TEST_VDTLLIST
