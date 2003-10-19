69aa3c7693256cfc54e7ca0f7338a626063622b8
Daniel Wilkerson <daniel.wilkerson@gmail.com>
2003-10-19 05:36:41 +0000

    minimized in/d0019.cc from nsCLiveconnectFactory.i

---
+++ elsa/in/d0019.cc
@@ -0,0 +1,19 @@
+// minimized from in/big/nsCLiveconnectFactory.i
+
+template <class T>
+struct D : T {};
+
+struct Q {};
+
+template <class T>
+struct C {
+  C (Q const &h);
+  operator D<T> *();
+};
+
+struct N {};
+
+Q d(N *a);
+int s(N *lhs, N *rhs) {
+  return C<N> (d (lhs)) == C<N> (d (rhs));
+}
