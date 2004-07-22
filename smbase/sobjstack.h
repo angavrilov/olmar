bbf79eaa9d3228b4a578ea25c05d7e31cdfbf1cf
Scott McPeak <smcpeak@cs.berkeley.edu>
2004-07-22 20:49:33 +0000

    added StackMaintainer

--- smbase/sobjstack.h
+++ smbase/sobjstack.h
@@ -29,4 +29,39 @@ public:       // funcs
   bool contains(T const *item) const    { return list.contains((void*)item); }
 };
 
+
+// utility class for maintaining a first-class sub-stack of the AST
+// stack isomorphic to the stackframe stack; Note that the fact that
+// nothing happens if 'obj' is NULL is a feature: sometimes you can't
+// map the need to call this class completely onto the control flow,
+// and so some dataflow is involved; since the dtor for this class is
+// used as a kind of finally statement, we can't nest its construction
+// in an 'if' statement!  Instead pass in NULL if you want a no-op
+// effect.
+template<class T>
+class StackMaintainer {
+  SObjStack<T> &s;
+  T *obj;
+
+  StackMaintainer(StackMaintainer&); // forbid copying
+
+public:
+  explicit StackMaintainer(SObjStack<T> &s0, T *obj0)
+    : s(s0)
+    , obj(obj0)
+  {
+    if (obj) {
+      s.push(obj);
+    }
+  }
+
+  ~StackMaintainer() {
+    if (obj) {
+      T *obj0 = s.pop();
+      xassert(obj0 == obj);
+    }
+  }
+};
+
+
 #endif // SOBJSTACK_H
