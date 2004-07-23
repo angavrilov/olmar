fadaf213ad03982a96075ac396bb063532eaccac
Daniel Wilkerson <daniel.wilkerson@gmail.com>
2004-07-23 02:05:13 +0000

    make some improvements to hash function 2: more random and more documentation

--- smbase/strhash.cc
+++ smbase/strhash.cc
@@ -32,8 +32,8 @@ STATICDEF unsigned StringHash::coreHash(char const *key)
   #endif // 0
 
 
-  #ifdef NELSON_HASH
-  #warning NELSON_HASH
+  #if HASH_ALG == 1
+  #warning Nelson hashfunction
   // this one is supposed to be better
   /* An excellent string hashing function.
      Adapted from glib's g_str_hash().
@@ -53,39 +53,107 @@ STATICDEF unsigned StringHash::coreHash(char const *key)
   return h;
 
 
-  #elif WILKERSON_GOLDSMITH_HASH
-  #warning WILKERSON_GOLDSMITH_HASH
+  #elif HASH_ALG == 2
+  #warning Wilkerson/Goldsmith hashfunction
   // dsw: A slighly faster and likely more random implementation
-  // invented in collaboration with Simon Goldsmith.  Note that this
-  // one assumes that strings are allocated on word boundaries
-  // otherwise you could segfault while going off the end.
+  // invented in collaboration with Simon Goldsmith.
   //
-  // Thanks to Matt Harren for this.  Supposedly gcc will sometimes
-  // recognize this and generate a single rotate instruction 'ROR'.
+  // Supposedly gcc will sometimes recognize this and generate a
+  // single rotate instruction 'ROR'.  Thanks to Matt Harren for this.
   // http://groups.google.com/groups?q=rorl+x86&start=10&hl=en&lr=&ie=UTF-8&oe=UTF-8&
   // selm=359954C9.3B354F0%40cartsys.com&rnum=11
   // http://www.privacy.nb.ca/cryptography/archives/coderpunks/new/1998-10/0096.html
   #define ROTATE(n, b) (n >> b) | (n << (32 - b))
 
+  // Note that UINT_MAX        = 4294967295U;
+  // source of primes: http://www.utm.edu/research/primes/lists/small/small.html
+  static unsigned const primeA = 1500450271U;
+  static unsigned const primeB = 2860486313U;
   // source of primes: http://www.utm.edu/research/primes/lists/2small/0bit.html
-  static unsigned const primeA = (1U<<30) - 173U; // prime
-  static unsigned const primeB = (1U<<29) -  43U; // prime
+  static unsigned const primeC = (1U<<31) -  99U;
+  static unsigned const primeD = (1U<<30) -  35U;
+
+  static int count = 0;
+  ++count;
+
   unsigned h = primeA;
-  for (; key[0] && key[1] && key[2] && key[3]; key += 4) {
-    h += *( (int*) key );
+
+  // Stride a word at a time.  Note that this works best (or works at
+  // all) if the string is 32-bit aligned.  This initial 'if' block is
+  // to prevent an extra unneeded rotate.
+  if (!key[0]) goto end0;
+  if (!key[1]) goto end1;
+  if (!key[2]) goto end2;
+  if (!key[3]) goto end3;
+  // No rotate here.
+  h += *( (unsigned *) key );
+  key += 4;
+  while (1) {
+    // invariant: when we get here, we are ready to rotate
+    if (!key[0]) {h = ROTATE(h, 5); goto end0;}
+    if (!key[1]) {h = ROTATE(h, 5); goto end1;}
+    if (!key[2]) {h = ROTATE(h, 5); goto end2;}
+    if (!key[3]) {h = ROTATE(h, 5); goto end3;}
     h = ROTATE(h, 5);
+    h += *( (unsigned *) key ); // on my machine plus is faster than xor
+    key += 4;
   }
+  xfailure("shouldn't get here");
 
-  if (*key == '\0') goto endHashing;
+  // invariant: when we get here we are ready to add
+end3:
   h += *key; h = ROTATE(h, 5); key += 1;
-  if (*key == '\0') goto endHashing;
+end2:
   h += *key; h = ROTATE(h, 5); key += 1;
-  if (*key == '\0') goto endHashing;
-  h += *key; h = ROTATE(h, 5); key += 1;
-//    xassert(*key == '\0');
+end1:
+  h += *key;                    // No rotate nor increment here.
+  #ifndef NDEBUG
+    key += 1;                   // this is only needed for the assertion below
+  #endif
+end0:
+  xassertdb(*key=='\0');
 
-endHashing:
+  // I will say for now that the property of hash functions that we
+  // want is that a change in any input bit has a 50% chance of
+  // inverting any output bit.
+  //
+  // At this point, compare this hash function to the Nelson hash
+  // function above.  In Nelson, everytime data is added, the
+  // accumulator, 'h', is "stirred" with a multiplication.  Since data
+  // is being added at the low byte and since multiplication
+  // propagates dependencies towards the high bytes and since after
+  // ever add there is at least one multiply, every byte has a chance
+  // to affect the value of every bit above the first byte.
+  //
+  // However, in this hash function we have saved time by simply
+  // "tiling" the data across the accumulator and at this point it
+  // hasn't been "stirred" at all.  Since most hashvalues are used by
+  // modding off some high bits, those bits have never had a chance to
+  // affect the final value, so some stirring is needed.  How much
+  // "stirring" do we need?
+  //
+  // Consider the 32-bit word in two halves, H and L.
+  //
+  // 1) With a single multiply, any bit of L "has a chance" to affect
+  // any bit in H.  The reverse is not true.
   h *= primeB;
+
+  // 2) We therefore swap H and L and multiply again.  Now, any bit in
+  // H has had a chance to affect any bit in L.  We are not done
+  // though, since the high order bits in H have not had a chance to
+  // affect the low order bits of H (yes H).  Please note however,
+  // that since L affected H and H affected L, the hight order bits of
+  // L *have* had a chance to affect the low order bits of L.
+  h = ROTATE(h, 16);
+  h *= primeC;
+
+  // 3) Therefore we swap H and L and multiply once again.  Now the
+  // high order bits of H have had a chance to affect L (in 2) which
+  // now can affect H again.  Any bit now has "had a chance" to affect
+  // any other bit.
+  h = ROTATE(h, 16);
+  h *= primeD;
+
   return h;
 
   #undef ROTATE
@@ -93,7 +161,7 @@ endHashing:
 
   #else
     #error You must pick a hash function
-  #endif // hash function multi-switch
+  #endif // HASH_ALG multi-switch
 }
 
 
