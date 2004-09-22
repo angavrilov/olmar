c1169b9fcca26ca10755b64e50360c593c7aa688
Daniel Wilkerson <daniel.wilkerson@gmail.com>
2004-09-22 18:37:07 +0000

    added minimized test

---
+++ elsa/in/d0111.cc
@@ -0,0 +1,8 @@
+// CoderInfo-ootm.ii:8:22: error: variable name `D</*ref*/ I0>::P' used as if it were a type
+
+template<int I> class D {
+  union P {};
+  static P *L[N];
+};
+
+template<int I0> D<I0>::P *D<I0>::L[N] = {0, 0};
