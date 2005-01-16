48edb288fc8221a4f1266140be0ac09ae92bb58b
Scott McPeak <smcpeak@cs.berkeley.edu>
2005-01-16 09:23:28 +0000

    added get_smbase_BASE_FLAGS

--- smbase/sm_config.pm
+++ smbase/sm_config.pm
@@ -10,6 +10,15 @@ sub get_sm_config_version {
   return 1.01;
 }
 
+# use smbase's $BASE_FLAGS if I can find them
+sub get_smbase_BASE_FLAGS {
+  my $smbase_flags = `$main::SMBASE/config.summary 2>/dev/null | grep BASE_FLAGS`;
+  if (defined($smbase_flags)) {
+    ($main::BASE_FLAGS = $smbase_flags) =~ s|^.*: *||;
+    chomp($main::BASE_FLAGS);
+  }
+}
+
 
 1;
 # EOF
