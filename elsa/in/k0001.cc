0b817d9d83bc79f1d64a28b0a899b4bd7f0e9441
Karl Chen <quarl@cs.berkeley.edu>
2005-03-28 02:51:03 +0000

    *** empty log message ***

--- elsa/in/k0001.cc
+++ elsa/in/k0001.cc
@@ -1,5 +1,8 @@
-// package: apt
-// file: sourcelist.cc
+// invoking a pointer to member function of a template class
+
+// originally found in:
+//   package: apt
+//   file: sourcelist.cc
 
 //ERROR: left side of .* must be a class or reference to a class
 template <class T> void foo() {
