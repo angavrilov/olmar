0ce27711ee185dd2b44eb3f3ad26e5c6aec8d1cb
Karl Chen <quarl@cs.berkeley.edu>
2005-03-30 12:47:48 +0000

    In/k0013.cc: cannot convert argument type `float (*)[4]' to parameter 1 type `float const (*)[4]'

---
+++ elsa/in/k0013.cc
@@ -0,0 +1,11 @@
+
+//ERROR: cannot convert argument type `float (*)[4]' to parameter 1 type `float const (*)[4]'
+
+// originally found in package xfree86.
+
+void bar(const float M[4][4]);
+
+void foo() {
+    float *m[4];
+    bar((float (*)[4]) m);
+}
