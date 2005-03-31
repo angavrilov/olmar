2da40e88c2431dd20cf6ae8947a5d332725d8bf2
Karl Chen <quarl@cs.berkeley.edu>
2005-03-31 08:01:18 +0000

    Assertion failed: !ambiguity, file cc.ast.gen.cc line 2726
    originally found in package aspell

---
+++ elsa/in/k0014.cc
@@ -0,0 +1,21 @@
+// Assertion failed: !ambiguity, file cc.ast.gen.cc line 2726
+
+// originally found in package aspell
+
+struct B {};
+
+template <typename T> struct C : B {
+    C() {}
+    C(C<int> & other) : B(other)
+    {
+        foo();
+    }
+
+    void foo() {}
+};
+
+
+int main() {
+    C<int> c1;
+    C<long> c2(c1);
+}
