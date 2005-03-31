9bb3d2c3d384b2efc4310782e481d5dd31fab05a
Karl Chen <quarl@cs.berkeley.edu>
2005-03-31 22:19:26 +0000

    template argument default value from another template class

---
+++ elsa/in/k0026.cc
@@ -0,0 +1,19 @@
+// template argument default value from another template class
+
+// originally found in package buffy
+
+// Assertion failed: newHead->next == NULL, file ../ast/fakelist.h line 81
+
+template <typename T> struct B {
+    static const int value = true;
+};
+
+struct T1 {
+};
+
+template <class U, int v = B<T1>::value>
+struct S {
+};
+
+struct U{};
+S<U> x;
