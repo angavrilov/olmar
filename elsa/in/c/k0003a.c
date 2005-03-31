effc189523299ebebe2538bc9754fdd691b58517
Karl Chen <quarl@cs.berkeley.edu>
2005-03-31 08:24:44 +0000

    old-style function decl with parentheses around func name, returning a struct-typedef
    "WARNING: there is no action to merge nonterm KandRFunctionDefinition"
    
    originally found in package gcl

---
+++ elsa/in/c/k0003a.c
@@ -0,0 +1,13 @@
+// old-style function decl with parentheses around func name, returning a
+// struct-typedef
+
+// "WARNING: there is no action to merge nonterm KandRFunctionDefinition"
+
+// originally found in package gcl
+
+typedef struct {} S;
+
+S (foo)(x)
+    int x;
+{
+}
