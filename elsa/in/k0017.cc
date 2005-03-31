847947bb9e609e67f6a95fec2c80c1748d4cd41a
Karl Chen <quarl@cs.berkeley.edu>
2005-03-31 11:35:51 +0000

    namespace std undefined before 'using'

---
+++ elsa/in/k0017.cc
@@ -0,0 +1,9 @@
+// namespace std undefined before 'using'
+
+//  [ This may be a gcc extension, though gcc -pedantic accepts it. --Karl ]
+
+// originally found in package amsynth
+
+using namespace std;
+
+//ERROR(1): using namespace BogusNamespace;
