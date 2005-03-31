0042121663f90daccf7f1715b24dcab88492daf6
Karl Chen <quarl@cs.berkeley.edu>
2005-03-31 08:53:48 +0000

    Elaborate with more test cases (another package failed with template version)

--- elsa/in/k0005.cc
+++ elsa/in/k0005.cc
@@ -1,13 +1,24 @@
 // declaring a constructor with class name
 
 // originally found in package groff
+// template version originally found in package aime
 
 // error: undeclared identifier `S::S'
 
 // error: the name `S::S' is overloaded, but the type `()(int x)' doesn't
 // match any of the 2 declar
 
-struct S {
-    S::S(int x);
+struct S1 {
+    S1::S1(int x) {}
 };
-S::S(int x) {}
+
+
+struct S2 {
+    S2::S2(int x);
+};
+S2::S2(int x) {}
+
+
+template<class T> struct S3 {
+    S3<T>::S3() {}
+};
