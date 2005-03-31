4a5b6fbe48e7ac442afde51401e8d3bb35efbe48
Karl Chen <quarl@cs.berkeley.edu>
2005-03-31 11:55:53 +0000

    member function taking an array argument with default value __null

---
+++ elsa/in/k0018.cc
@@ -0,0 +1,9 @@
+// member function taking an array argument with default value __null
+
+// Assertion failed: isArrayType(), file cc_type.cc line 870
+
+// originally found in package anjuta
+
+struct S {
+    void f(int a[] = __null);
+};
