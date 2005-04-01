9a583816919e3a0d0ae3955088d6ac55c8e94c86
Karl Chen <quarl@cs.berkeley.edu>
2005-04-01 18:35:45 +0000

    Added failparse k0041.cc "inner struct defined inline in an inner struct not defined inline"

---
+++ elsa/in/k0041.cc
@@ -0,0 +1,16 @@
+// inner struct defined inline in an inner struct not defined inline
+
+// originally found in package kdelibs_4:3.3.1-4
+
+// Assertion failed: (unsigned)i < (unsigned)sz, file ../smbase/array.h line 66
+
+struct S1 {
+    struct S2;
+};
+
+struct S1::S2 {
+    struct S3 {
+    };
+};
+
+S1 s1;
