b899b9ef758b433d1f86e4838fd0e3dd867cdcd1
Karl Chen <quarl@cs.berkeley.edu>
2005-04-01 09:55:32 +0000

    Added failparse k0040.cc "instantiating a template with unnamed template parameter"

---
+++ elsa/in/k0040.cc
@@ -0,0 +1,10 @@
+// instantiating a template with unnamed template parameter
+
+// originally found in package akregator_1.0-beta8-2
+
+// Assertion failed: key != NULL, file vptrmap.cc line 58
+
+template <class> struct S1;
+
+typedef S1<int> s1;
+
