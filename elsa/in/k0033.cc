ee90b2e8569362e7e427aed0617a8ba2b57c874b
Karl Chen <quarl@cs.berkeley.edu>
2005-04-01 01:44:54 +0000

    casting function pointer with throw() to/from no throw

---
+++ elsa/in/k0033.cc
@@ -0,0 +1,35 @@
+// casting function pointer with throw() to/from no throw
+
+// originally found in package abiword
+
+// error: cannot convert argument type `int (*&)() throw()' to parameter 1
+// type `int (*)()'
+
+int bar1 (int (*func) ());
+int bar2 (int (*func) () throw());
+
+int foo1 ();
+int foo2 () throw ();
+
+int main() {
+    // bar(foo);
+
+    int (*func1) () throw();
+
+    int (*func2) ();
+
+    func1 = func2;
+    func2 = func1;
+
+    bar1(func1);
+    bar1(func2);
+
+    bar2(func1);
+    bar2(func2);
+
+    bar1(foo1);
+    bar1(foo2);
+
+    bar2(foo1);
+    bar2(foo2);
+}
