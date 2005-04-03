d1586c100b02cec9ead3a10b345c72c69ca24145
Karl Chen <quarl@cs.berkeley.edu>
2005-04-03 01:48:07 +0000

    (Comment)

--- elsa/in/k0045.cc
+++ elsa/in/k0045.cc
@@ -2,11 +2,9 @@
 
 // originally found in package drscheme_1:208-1
 
-// %%% progress: 0ms: done type checking (1 ms)
-// a.ii:3:6: error: prior declaration of `operator delete' at <init>:1:1 had type `void ()(void *p) throw()', but this one uses `void ()(void */*anon*/)'
-// typechecking results:
-//   errors:   1
-//   warnings: 0
+// a.ii:3:6: error: prior declaration of `operator delete' at <init>:1:1 had
+// type `void ()(void *p) throw()', but this one uses `void ()(void
+// */*anon*/)'
 
 void operator delete(void *) {
 }
