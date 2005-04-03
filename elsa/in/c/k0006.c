70a2c506647cb7361f27f7bbd560cd9e3b0c5895
Karl Chen <quarl@cs.berkeley.edu>
2005-04-03 02:24:43 +0000

    (Comment)

--- elsa/in/c/k0006.c
+++ elsa/in/c/k0006.c
@@ -2,8 +2,6 @@
 
 // originally found in package framerd_2.4.1-1.1
 
-// In state 954, I expected one of these tokens:
-//   (, =, ,, ;,
 // a.i:4:1: Parse error (state 954) at {
 
 static inline foo()
