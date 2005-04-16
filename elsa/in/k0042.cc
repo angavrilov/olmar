bec69eb3ffa443b5b5df3b4876cc2996e060dc93
Scott McPeak <smcpeak@cs.berkeley.edu>
2005-04-16 07:46:02 +0000

    allow in/k0042.cc (really a gnu extension) by allowing assignment expressions in the array dimension slot

--- elsa/in/k0042.cc
+++ elsa/in/k0042.cc
@@ -10,6 +10,20 @@
 int main() {
     int i;
     char str[i=4];
+    
+    // does gcc/icc allow commas too?
+    //
+    // interesting; gcc does *not*, while icc *does*!
+    //
+    // I will reject
+    //ERROR(1): char str2[4/*ignored*/, 6];
+    
+    // what about throw?  they both parse it but then reject
+    // for tcheck reasons; I will do the same (since it is the
+    // behavior that naturally falls out of my implementation)
+    //
+    // er, I guess my tchecker isn't strict enough
+    //char str3[throw 4];
 
     return 0;
 }
