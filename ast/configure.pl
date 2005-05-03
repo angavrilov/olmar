030fde57b77708f08679fe8888cad00bd7b53316
Scott McPeak <smcpeak@cs.berkeley.edu>
2005-05-03 20:10:56 +0000

    redesigned/expanded configure script for mingw32 cross-compile

--- ast/configure.pl
+++ ast/configure.pl
@@ -5,7 +5,8 @@ use strict 'subs';
 
 # default location of smbase relative to this package
 $SMBASE = "../smbase";
-$req_smcv = 1.01;            # required sm_config version number
+$req_smcv = 1.03;            # required sm_config version number
+$thisPackage = "ast";
 
 # -------------- BEGIN common block ---------------
 # do an initial argument scan to find if smbase is somewhere else
@@ -39,80 +40,43 @@ if ($smcv < $req_smcv) {
 # -------------- END common block ---------------
 
 
-# defaults
-$BASE_FLAGS = "-Wall -Wno-deprecated -D__UNIX__";
-@CCFLAGS = ();
-$debug = 0;
-$use_dash_g = 1;
-$allow_dash_O2 = 1;
-
 sub usage {
+  standardUsage();
+
   print(<<"EOF");
-usage: ./configure [options]
-options:
-  -h:                print this message
-  -debug,-nodebug:   enable/disable debugging options [disabled]
-  -no-dash-g         disable -g
-  -no-dash-O2        disable -O2
+package options:
   -prof              enable profiling
   -devel             add options useful while developing
-  -ccflag <arg>      add <arg> to gcc command line
-  -smbase=<dir>:     specify where the smbase library is [$SMBASE]
 EOF
 }
 
 
+# -------------- BEGIN common block 2 -------------
+# global variables holding information about the current command-line
+# option being processed
+$option = "";
+$value = "";
+
 # process command-line arguments
-$originalArgs = join(' ', @ARGV);
-while (@ARGV) {      
-  my $tmp;
-  my $arg = $ARGV[0];
-  shift @ARGV;
+foreach $optionAndValue (@ARGV) {
+  # ignore leading '-' characters, and split at first '=' (if any)
+  ($option, $value) =
+    ($optionAndValue =~ m/^-*([^-][^=]*)=?(.*)$/);
+                      #      option     = value
 
-  # treat leading "--" uniformly with leading "-"
-  $arg =~ s/^--/-/;
+  my $arg = $option;
 
-  if ($arg eq "-h" ||
-      $arg eq "-help") {
-    usage();
-    exit(0);
+  if (handleStandardOption()) {
+    # handled by sm_config.pm
   }
+  # -------------- END common block 2 -------------
 
-  # things that look like options to gcc should just
-  # be added to CCFLAGS
-  elsif ($arg =~ m/^(-W|-pg$|-D|-O)/) {
-    push @CCFLAGS, $arg;
-  }
-  elsif ($arg eq "-ccflag") {
-    push @CCFLAGS, $ARGV[0];
-    shift @ARGV;
-  }
-
-  elsif ($arg eq "-d" ||
-         $arg eq "-debug") {
-    $debug = 1;
-  }
-  elsif ($arg eq "-nodebug") {
-    $debug = 0;
-  }
-
-  elsif ($arg eq "-no-dash-g") {
-    $use_dash_g = 0;
-  }
-  elsif ($arg eq "-no-dash-O2") {
-    $allow_dash_O2 = 0;
-  }
-
-  elsif ($arg eq "-prof") {
+  elsif ($arg eq "prof") {
     push @CCFLAGS, "-pg";
   }
 
-  elsif ($arg eq "-devel") {
+  elsif ($arg eq "devel") {
     push @CCFLAGS, "-Werror";
-  }
-
-  elsif (($tmp) = ($arg =~ m/^-smbase=(.*)$/)) {
-    $SMBASE = $tmp;
   }
 
   else {
@@ -120,133 +84,34 @@ while (@ARGV) {
   }
 }
 
-if (!$debug) {
-  if ($allow_dash_O2) {
-    push @CCFLAGS, "-O2";
-  }
-  push @CCFLAGS, "-DNDEBUG";
-}
-
-if ($use_dash_g) {
-  unshift @CCFLAGS, "-g";
-}
-
-$os = `uname -s`;
-chomp($os);
-if ($os eq "Linux") {
-  push @CCFLAGS, "-D__LINUX__";
-}
-
-# smash the list together to make a string
-$CCFLAGS = join(' ', @CCFLAGS);
+finishedOptionProcessing();
 
 
 # ------------------ check for needed components ----------------
-# smbase
-if (! -f "$SMBASE/nonport.h") {
-  die "I cannot find nonport.h in `$SMBASE'.\n" .
-      "The smbase library is required for ast.\n" .
-      "If it's in a different location, use the -smbase=<dir> option.\n";
-}
+test_smbase_presence();
 
-# I previously had a check for libsmbase.a, but that prevents
-# configuring all software from one master configure script, so
-# I've removed that.
+test_CXX_compiler();
 
-# use smbase's $BASE_FLAGS if I can find them
-get_smbase_BASE_FLAGS();
-
-
-# etags: see elsa/configure.pl
-
-
-# ---------------- misc -----------------
 $PERL = get_PERL_variable();
 
 
 # ------------------ config.summary -----------------
-# create a program to summarize the configuration
-open(OUT, ">config.summary") or die("can't make config.summary");
-print OUT (<<"EOF");
-#!/bin/sh
-# config.summary
-
-echo "./configure command:"
-echo "  $0 $originalArgs"
-echo ""
-echo "ast configuration summary:"
-echo "  debug:       $debug"
-echo ""
-echo "Compile flags:"
-echo "  BASE_FLAGS:  $BASE_FLAGS"
-echo "  CCFLAGS:     $CCFLAGS"
-echo "  SMBASE:      $SMBASE"
-echo ""
-EOF
-
-close(OUT) or die;
-chmod 0755, "config.summary";
+$summary = getStandardConfigSummary();
+writeConfigSummary($summary);
 
 
 # ------------------- config.status ------------------
-# from here on, combine BASE_FLAGS and CCFLAGS
-$CCFLAGS = "$BASE_FLAGS $CCFLAGS";
-
-# create a program which will create the Makefile
-open(OUT, ">config.status") or die("can't make config.status");
-print OUT (<<"__OUTER_EOF__");
-#!/bin/sh
-# config.status
-
-# this file was created by ./configure
-
-# report on configuration
-./config.summary
-
-echo "creating Makefile ..."
-
-# overcome my chmod below
-rm -f Makefile
-
-cat >Makefile <<EOF
-# Makefile for ast
-# NOTE: do not edit; generated by:
-#   $0 $originalArgs
-
-EOF
-
-
-# substitute variables
-sed -e "s|\@CCFLAGS\@|$CCFLAGS|g" \\
-    -e "s|\@SMBASE\@|$SMBASE|g" \\
-    -e "s|\@PERL\@|$PERL|g" \\
-  <Makefile.in >>Makefile || exit
-
-# discourage editing ..
-chmod a-w Makefile
-
-
-__OUTER_EOF__
-
-close(OUT) or die;
-chmod 0755, "config.status";
+writeConfigStatus("PERL" => "$PERL");
 
 
 # ----------------- final actions -----------------
 # run the output file generator
-my $code = system("./config.status");
-if ($code != 0) {
-  # hopefully ./config.status has already printed a message,
-  # I'll just relay the status code
-  if ($code >> 8) {                
-    exit($code >> 8);
-  }
-  else {
-    exit($code & 127);
-  }
-}
-
+run("./config.status");
 
 print("\nYou can now run make, usually called 'make' or 'gmake'.\n");
 
 exit(0);
+
+
+# silence warnings
+pretendUsed($thisPackage);
