40476bd37c8197890fcb2981eea292e790659561
Daniel Wilkerson <daniel.wilkerson@gmail.com>
2005-05-18 23:01:51 +0000

    explained a test failure

--- elsa/in/c/dC0023.c
+++ elsa/in/c/dC0023.c
@@ -1,2 +1,13 @@
+// in gcc, in C mode, a foo and a struct foo have nothing to do with
+// one another
 typedef char *foo;
 struct foo *q;
+
+// the test exhibits the bug without the below, but the below extends
+// the test
+int main() {
+  struct foo *gronk = q;
+  foo zork;
+  char *bork = zork;
+}
+
