9a0a1e4eaf50165899e310d9e99f6d64972e1d00
Scott McPeak <smcpeak@cs.berkeley.edu>
2005-05-27 01:51:06 +0000

    fixed in/c/dC0026.c, implicit int in K&R param declaration, by adding another production, but introduced an unhandled ambiguity (in/c/t0023.c), which I am just leaving for now, since fixing it seems to require adding a new ambiguity link...

--- elsa/in/c/dC0026.c
+++ elsa/in/c/dC0026.c
@@ -1,5 +1,8 @@
-pack12 (p, n, last)
+// dC0026.c
+// implicit int in K&R decl
+
+void pack12 (p, n, last)
      register char *p;
-     register n;
-{
-}
+     register /*implicit-int*/ n;
+     int last;
+{}
