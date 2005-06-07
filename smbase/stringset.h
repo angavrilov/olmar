2023018ab930b3237e4c8de5f619182b4e06490a
Daniel Wilkerson <daniel.wilkerson@gmail.com>
2005-06-07 21:32:02 +0000

    added iterators

--- smbase/stringset.h
+++ smbase/stringset.h
@@ -15,6 +15,43 @@ public:      // funcs
   StringSet() : elts() {}
   ~StringSet();
 
+  // external iterator
+  class Iter {
+  private:
+    StringSObjDict<int>::Iter iter;
+
+  public:
+    Iter(StringSet &set) : iter(set.elts) {}
+    Iter(Iter const &obj) : DMEMB(iter) {}
+    Iter& operator= (Iter const &obj) { CMEMB(iter); return *this; }
+
+    bool isDone() const { return iter.isDone(); }
+    Iter& next() { iter.next(); return *this; }
+
+    string const &data() const { return iter.key(); }
+
+    int private_getCurrent() const { return iter.private_getCurrent(); }
+  };
+  friend class Iter;
+
+  class IterC {
+  private:
+    StringSObjDict<int>::IterC iter;
+
+  public:
+    IterC(StringSet const &set) : iter(set.elts) {}
+    IterC(IterC const &obj) : DMEMB(iter) {}
+    IterC& operator= (IterC const &obj) { CMEMB(iter); return *this; }
+
+    bool isDone() const { return iter.isDone(); }
+    IterC& next() { iter.next(); return *this; }
+
+    string const &data() const { return iter.key(); }
+
+    int private_getCurrent() const { return iter.private_getCurrent(); }
+  };
+  friend class IterC;
+
   // # elts in the set
   int size() const                        { return elts.size(); }
 
@@ -36,5 +73,11 @@ public:      // funcs
   // empty the set
   void empty()                            { elts.empty(); }
 };
+
+#define FOREACH_STRINGSET(list, iter) \
+  for(StringSet::IterC iter(list); !iter.isDone(); iter.next())
+
+#define FOREACH_STRINGSET_NC(list, iter) \
+  for(StringSet::Iter iter(list); !iter.isDone(); iter.next())
 
 #endif // STRINGSET_H
