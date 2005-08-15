4032970f0aae9bc88635d9a2a2dafd7d170ef470
Daniel Wilkerson <daniel.wilkerson@gmail.com>
2005-08-15 20:56:22 +0000

    changed from using long long and atoll() to strtol() and strtoul(), per Scott's suggestion that it makes the code more portable; it is also less heavy-handed as we only compute into the type we ultimately want

--- ast/xmlhelp.cc
+++ ast/xmlhelp.cc
@@ -1,18 +1,6 @@
 #include "xmlhelp.h"            // this module
 #include <stdlib.h>             // atof, atol
 
-// dsw: NOTE: I don't know where to put this so I'll put it here.  I
-// de-serialize a string of digits representing an int of some kind
-// using atoll(), which returns a long long, which I have named here
-// intParseTarget_t.  The reason is that an unsigned int actually can
-// go higher than a signed long, so de-serializing with atol() is not
-// sufficient when the result is to be stored in an unsigned it: if
-// the number is greater than LONG_MAX then it becomes LONG_MAX.
-// However, all this will fail if we compile on a platform where long
-// long is just as wide as a long.
-typedef long long intParseTarget_t;
-#define parseInt(X) atoll(X)
-
 string toXml_bool(bool b) {
   if (b) return "true";
   else return "false";
@@ -28,7 +16,7 @@ string toXml_int(int i) {
 }
 
 void fromXml_int(int &i, rostring str) {
-  intParseTarget_t i0 = parseInt(str.c_str());
+  long i0 = strtol(str.c_str(), NULL, 10);
   i = i0;
 }
 
@@ -38,7 +26,7 @@ string toXml_long(long i) {
 }
 
 void fromXml_long(long &i, rostring str) {
-  intParseTarget_t i0 = parseInt(str.c_str());
+  long i0 = strtol(str.c_str(), NULL, 10);
   i = i0;
 }
 
@@ -48,7 +36,7 @@ string toXml_unsigned_int(unsigned int i) {
 }
 
 void fromXml_unsigned_int(unsigned int &i, rostring str) {
-  intParseTarget_t i0 = parseInt(str.c_str());
+  unsigned long i0 = strtoul(str.c_str(), NULL, 10);
   i = i0;
 }
 
@@ -58,7 +46,7 @@ string toXml_unsigned_long(unsigned long i) {
 }
 
 void fromXml_unsigned_long(unsigned long &i, rostring str) {
-  intParseTarget_t i0 = parseInt(str.c_str());
+  unsigned long i0 = strtoul(str.c_str(), NULL, 10);
   i = i0;
 }
 
