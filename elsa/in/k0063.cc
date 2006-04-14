6ca511697b4458e6422292338f1db5869927c0f7
Karl Chen <quarl@cs.berkeley.edu>
2006-04-14 05:48:44 +0000

    new test in/k0063.cc
    // Assertion failed: prior->hasFlag(DF_EXTERN_C), file cc_env.cc line 3505
    // Failure probably related to code near k0063.cc:10:14
    // current location stack:
    //   k0063.cc:10:14
    //   k0063.cc:9:1
    
    // ERR-MATCH: Assertion failed: prior->hasFlag.DF_EXTERN_C.
    
    struct S {
      friend int main(int argc, char* argv[]);
    };

---
+++ elsa/in/k0063.cc
@@ -0,0 +1,16 @@
+// class friend main
+
+// originally found in package 'bibletime'.
+
+// Assertion failed: prior->hasFlag(DF_EXTERN_C), file cc_env.cc line 3505
+// Failure probably related to code near k0063.cc:10:14
+// current location stack:
+//   k0063.cc:10:14
+//   k0063.cc:9:1
+
+// ERR-MATCH: Assertion failed: prior->hasFlag.DF_EXTERN_C.
+
+struct S {
+  friend int main(int argc, char* argv[]);
+};
+
