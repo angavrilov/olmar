fb856f7698f67be65e1dec104713621567b2e1f4
Karl Chen <quarl@cs.berkeley.edu>
2006-04-19 02:39:54 +0000

    Stricter ERR-MATCH

--- elsa/in/c/k0013.c
+++ elsa/in/c/k0013.c
@@ -4,7 +4,7 @@
 
 // a.i:7:13: error: prior declaration of `foo' at a.i:5:12 had type `int', but this one uses `enum MyEnum'
 
-// ERR-MATCH: prior declaration of `.*?' at .*? had type `.*?', but this one uses `.*?'
+// ERR-MATCH: prior declaration of `.*?' at .*? had type `int', but this one uses `enum .*?'
 
 enum MyEnum { DUMMY_WHICH_MUST_BE_NEGATIVE = -1, DUMMY2 };
 
