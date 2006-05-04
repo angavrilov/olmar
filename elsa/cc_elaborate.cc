120899782ea710320b9130576b519cb0b94d2fcb
Karl Chen <quarl@cs.berkeley.edu>
2006-05-04 07:34:13 +0000

    Turn segfault into assertion failure (48c58fcf-900a-4b17-bf13-17a2c93d799d); see in/c/k0016.c

--- elsa/cc_elaborate.cc
+++ elsa/cc_elaborate.cc
@@ -519,7 +519,7 @@ void Declarator::elaborateCDtors(ElabVisitor &env, DeclFlags dflags)
       }
       ASTENDCASED
     }
-    
+
     // According to the semantics of E_constructor, it is legal for
     // 'ctor' to be NULL here, as it signifies the use of a trivial
     // constructor.
@@ -1893,6 +1893,10 @@ bool ElabVisitor::visitExpression(Expression *e)
     // literals in 'asm's.
     return false;
   }
+
+  // e->type is NULL when we're inside a compound initializer array index?
+  // see in/c/k0016.c
+  xassert (e->type && "48c58fcf-900a-4b17-bf13-17a2c93d799d");
 
   // don't elaborate template-dependent expressions
   //
