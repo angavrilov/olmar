c6cac17e2e3cb17881cc705c2b06d2d3b122bc94
Karl Chen <quarl@cs.berkeley.edu>
2006-06-03 23:23:36 +0000

    Revert change to GrowArray and instead add new class BitwiseGrowArray to do this.  New class BitwiseArrayStack that uses BitwiseGrowArray.

--- smbase/array.h
+++ smbase/array.h
@@ -47,6 +47,7 @@ public:
 };
 
 
+
 // ------------------ GrowArray --------------------
 // This class implements an array of T's; it automatically expands
 // when 'ensureAtLeast' or 'ensureIndexDoubler' is used; it does not
@@ -54,16 +55,8 @@ public:
 //
 // class T must have:
 //   T::T();           // default ctor for making arrays
+//   operator=(T&);    // assignment for copying to new storage
 //   T::~T();          // dtor for when old array is cleared
-//
-// to use copyFrom, T must have:
-//   T::operator=(T const&);
-//
-// quarl 2006-06-01
-//   GrowArray uses malloc/free and manually calls T() and ~T(), so that we
-//   don't need to have T::operator=(T&); this is also faster since we only do
-//   a shallow copy.
-
 template <class T>
 class GrowArray {
 private:     // data
@@ -140,6 +133,188 @@ public:      // funcs
       arr[i] = val;
     }
   }
+};
+
+
+template <class T>
+GrowArray<T>::GrowArray(int initSz)
+{
+  sz = initSz;
+  if (sz > 0) {
+    arr = new T[sz];
+  }
+  else {
+    arr = NULL;
+  }
+}
+
+
+template <class T>
+GrowArray<T>::~GrowArray()
+{
+  if (arr) {
+    delete[] arr;
+  }
+}
+
+
+template <class T>
+void GrowArray<T>::copyFrom_limit(GrowArray<T> const &obj, int limit)
+{
+  for (int i=0; i<limit; i++) {
+    arr[i] = obj.arr[i];
+  }
+}
+
+
+template <class T>
+void GrowArray<T>::setSize(int newSz)
+{
+  if (newSz != sz) {
+    // keep track of old
+    int oldSz = sz;
+    T *oldArr = arr;
+
+    // make new
+    sz = newSz;
+    if (sz > 0) {
+      arr = new T[sz];
+    }
+    else {
+      arr = NULL;
+    }
+
+    // copy elements in common
+    for (int i=0; i<sz && i<oldSz; i++) {
+      arr[i] = oldArr[i];
+    }
+
+    // get rid of old
+    if (oldArr) {
+      delete[] oldArr;
+    }
+  }
+}
+
+
+// this used to be ensureIndexDoubler's implementation, but
+// I wanted the very first check to be inlined
+template <class T>
+void GrowArray<T>::eidLoop(int index)
+{
+  if (sz-1 >= index) {
+    return;
+  }
+
+  int newSz = sz;
+  while (newSz-1 < index) {
+    #ifndef NDEBUG_NO_ASSERTIONS    // silence warning..
+      int prevSz = newSz;
+    #endif
+    if (newSz == 0) {
+      newSz = 1;
+    }
+    newSz = newSz*2;
+    xassert(newSz > prevSz);        // otherwise overflow -> infinite loop
+  }
+
+  setSize(newSz);
+}
+
+
+// ------------------ BitwiseGrowArray --------------------
+// This class implements an array of T's; it automatically expands
+// when 'ensureAtLeast' or 'ensureIndexDoubler' is used; it does not
+// automatically contract.  All accesses are bounds-checked.
+//
+// class T must have:
+//   T::T();           // default ctor for making arrays
+//   T::~T();          // dtor for when old array is cleared
+//
+// to use copyFrom, T must have:
+//   T::operator=(T const&);
+//
+// quarl 2006-06-01
+//   BitwiseGrowArray uses malloc/free and manually calls T() and ~T(), so that we
+//   don't need to have T::operator=(T&); this is also faster since we only do
+//   a shallow copy.
+
+template <class T>
+class BitwiseGrowArray {
+private:     // data
+  T *arr;                 // underlying array; NULL if sz==0
+  int sz;                 // # allocated entries in 'arr'
+
+private:     // funcs
+  void bc(int i) const    // bounds-check an index
+    { xassert((unsigned)i < (unsigned)sz); }
+  void eidLoop(int index);
+
+  // make 'this' equal to 'obj'
+  void copyFrom(BitwiseGrowArray<T> const &obj) {
+    setSize(obj.size());     // not terribly efficient, oh well
+    copyFrom_limit(obj, sz);
+  }
+
+protected:   // funcs
+  void copyFrom_limit(BitwiseGrowArray<T> const &obj, int limit);
+
+private:     // disallowed
+  void operator=(BitwiseGrowArray&);
+  void operator==(BitwiseGrowArray&);
+
+public:      // funcs
+  BitwiseGrowArray(int initSz);
+  BitwiseGrowArray(BitwiseGrowArray const &obj) : arr(0), sz(0) { copyFrom(obj); }
+  ~BitwiseGrowArray();
+
+  BitwiseGrowArray& operator=(BitwiseGrowArray const &obj) { copyFrom(obj); return *this; }
+
+  // allocated space
+  int size() const { return sz; }
+
+  // element access
+  T const& operator[] (int i) const   { bc(i); return arr[i]; }
+  T      & operator[] (int i)         { bc(i); return arr[i]; }
+
+  // set size, reallocating if old size is different; if the
+  // array gets bigger, existing elements are preserved; if the
+  // array gets smaller, elements are truncated
+  void setSize(int newSz);
+
+  // make sure there are at least 'minSz' elements in the array;
+  void ensureAtLeast(int minSz)
+    { if (minSz > sz) { setSize(minSz); } }
+
+  // grab a read-only pointer to the raw array
+  T const *getArray() const { return arr; }
+
+  // grab a writable pointer; use with care
+  T *getDangerousWritableArray() { return arr; }
+  T *getArrayNC() { return arr; }     // ok, not all that dangerous..
+
+  // make sure the given index is valid; if this requires growing,
+  // do so by doubling the size of the array (repeatedly, if
+  // necessary)
+  void ensureIndexDoubler(int index)
+    { if (sz-1 < index) { eidLoop(index); } }
+
+  // set an element, using the doubler if necessary
+  void setIndexDoubler(int index, T const &value)
+    { ensureIndexDoubler(index); arr[index] = value; }
+
+  // swap my data with the data in another BitwiseGrowArray object
+  void swapWith(BitwiseGrowArray<T> &obj) {
+    T *tmp1 = obj.arr; obj.arr = this->arr; this->arr = tmp1;
+    int tmp2 = obj.sz; obj.sz = this->sz; this->sz = tmp2;
+  }
+
+  // set all elements to a single value
+  void setAll(T val) {
+    for (int i=0; i<sz; i++) {
+      arr[i] = val;
+    }
+  }
 
 private:
   // call T constructor on [p, end)
@@ -176,7 +351,7 @@ private:
 
 
 template <class T>
-GrowArray<T>::GrowArray(int initSz)
+BitwiseGrowArray<T>::BitwiseGrowArray(int initSz)
 {
   sz = initSz;
   if (sz > 0) {
@@ -191,7 +366,7 @@ GrowArray<T>::GrowArray(int initSz)
 
 
 template <class T>
-GrowArray<T>::~GrowArray()
+BitwiseGrowArray<T>::~BitwiseGrowArray()
 {
   if (arr) {
     // delete[] arr;
@@ -202,7 +377,7 @@ GrowArray<T>::~GrowArray()
 
 
 template <class T>
-void GrowArray<T>::copyFrom_limit(GrowArray<T> const &obj, int limit)
+void BitwiseGrowArray<T>::copyFrom_limit(BitwiseGrowArray<T> const &obj, int limit)
 {
   for (int i=0; i<limit; i++) {
     arr[i] = obj.arr[i];
@@ -211,7 +386,7 @@ void GrowArray<T>::copyFrom_limit(GrowArray<T> const &obj, int limit)
 
 
 template <class T>
-void GrowArray<T>::setSize(int newSz)
+void BitwiseGrowArray<T>::setSize(int newSz)
 {
   if (newSz != sz) {
     // keep track of old
@@ -256,7 +431,7 @@ void GrowArray<T>::setSize(int newSz)
 // this used to be ensureIndexDoubler's implementation, but
 // I wanted the very first check to be inlined
 template <class T>
-void GrowArray<T>::eidLoop(int index)
+void BitwiseGrowArray<T>::eidLoop(int index)
 {
   if (sz-1 >= index) {
     return;
@@ -410,6 +585,95 @@ public:
   ~ArrayStackPopper()
     { stk.pop(); }
 };
+
+
+// ---------------------- BitwiseArrayStack ---------------------
+// quarl 2006-06-03
+// Analogous to ArrayStack, but uses a BitwiseGrowArray instead of GrowArray.
+// TODO: make ArrayStack templatized on the base class
+template <class T>
+class BitwiseArrayStack : public BitwiseGrowArray<T> {
+private:
+  int len;               // # of elts in the stack
+
+public:
+  BitwiseArrayStack(int initArraySize = 10)
+    : BitwiseGrowArray<T>(initArraySize),
+      len(0)
+    {}
+  BitwiseArrayStack(BitwiseArrayStack<T> const &obj)
+    : BitwiseGrowArray<T>(obj),
+      len(obj.len)
+    {}
+  ~BitwiseArrayStack() {}
+
+  // copies contents of 'obj', but the allocated size of 'this' will
+  // only change when necessary
+  BitwiseArrayStack& operator=(BitwiseArrayStack<T> const &obj)
+  {
+    this->ensureIndexDoubler(obj.length() - 1);
+    this->copyFrom_limit(obj, obj.length());
+    len = obj.len;
+    return *this;
+  }
+
+  // element access; these declarations are necessary because
+  // the uses of 'operator[]' below are not "dependent", hence
+  // they can't use declarations inherited from GrowArray<T>
+  T const& operator[] (int i) const { return BitwiseGrowArray<T>::operator[](i); }
+  T      & operator[] (int i)       { return BitwiseGrowArray<T>::operator[](i); }
+
+  void push(T const &val)
+    { setIndexDoubler(len++, val); }
+  T pop()
+    { return operator[](--len); }
+  T const &top() const
+    { return operator[](len-1); }
+  T &top()
+    { return operator[](len-1); }
+  T &nth(int which)
+    { return operator[](len-1-which); }
+
+  // alternate interface, where init/deinit is done explicitly
+  // on returned references
+  T &pushAlt()    // returns newly accessible item
+    { BitwiseGrowArray<T>::ensureIndexDoubler(len++); return top(); }
+  T &popAlt()     // returns item popped
+    { return operator[](--len); }
+
+  // items stored
+  int length() const
+    { return len; }
+
+  bool isEmpty() const
+    { return len==0; }
+  bool isNotEmpty() const
+    { return !isEmpty(); }
+
+  void popMany(int ct)
+    { len -= ct; xassert(len >= 0); }
+  void empty()
+    { len = 0; }
+
+  // useful when someone has used 'getDangerousWritableArray' to
+  // fill the array's internal storage
+  void setLength(int L) { len = L; }
+
+  // consolidate allocated space to match length
+  void consolidate() { this->setSize(length()); }
+
+  // swap
+  void swapWith(BitwiseArrayStack<T> &obj) {
+    BitwiseGrowArray<T>::swapWith(obj);
+    int tmp = obj.len; obj.len = this->len; this->len = tmp;
+  }
+
+  void sort(int (*compare)(T const *t1, T const *t2)) {
+    qsort(BitwiseGrowArray<T>::getArrayNC(), len, sizeof(T),
+          (int (*)(void const*, void const*))compare );
+  }
+};
+
 
 template <class T> class ObjArrayStackIterNC;
 
