a5f987876c242de3e03b028e9826f2e443812c20
Karl Chen <quarl@cs.berkeley.edu>
2006-06-14 22:46:40 +0000

    Err-match

--- elsa/in/k0110.cc
+++ elsa/in/k0110.cc
@@ -5,6 +5,8 @@
 // a.ii:9:8: error: call site name lookup failed to yield any candidates; last
 // candidate was removed because: non-template given template arguments
 
+//ERR-MATCH: last candidate was removed because: non-template given template arguments
+
 template<typename T>
 struct S {
 };
