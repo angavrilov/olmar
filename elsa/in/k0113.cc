c3e9819a9464b7aac5c6defe0d22ae1fa6671fb5
Karl Chen <quarl@cs.berkeley.edu>
2006-06-15 00:16:21 +0000

    New failing test case
    +failparse k0113.cc "calling templatized operator-overloaded function"

---
+++ elsa/in/k0113.cc
@@ -0,0 +1,34 @@
+// calling templatized operator-overloaded function
+
+// In state 161, I expected one of these tokens:
+//   <name>,
+// b.ii:15:22: Parse error (state 161) at operator
+
+// from sigc++-2.0/sigc++/functors/slot.h
+
+template <class T>
+struct S1 {
+    template <class T2>
+    void operator()() {}
+
+    template <class T2>
+    void foo() {}
+};
+
+template <class T>
+struct S2 {
+    void foo() {
+        S1<T> *s1;
+        s1->template operator()<int> ();
+    }
+};
+
+int main()
+{
+    S1<int> s1;
+    s1.foo<int>();
+    s1.operator()<int>();
+
+    S2<int> s2;
+    s2.foo();
+}
