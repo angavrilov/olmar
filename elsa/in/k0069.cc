41ca48e478b3a4c19e7b36b6774171cf74f8a527
Karl Chen <quarl@cs.berkeley.edu>
2006-06-16 03:54:09 +0000

    Undo previous - hard to distinguish errors

--- elsa/in/k0069.cc
+++ elsa/in/k0069.cc
@@ -7,7 +7,7 @@
 //   warnings: 0
 // error: k0069.cc:15:12: internal error: found dependent type `Friend' in non-template
 
-// ERR-MATCH: internal error: found dependent type `[^(].*?' in non-template
+// ERR-MATCH: internal error: found dependent type `.*?' in non-template
 
 template< class Type > struct T {
   friend struct Friend ;
