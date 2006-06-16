328c3480dfdc9bac308444a31e40dcdea4a95246
Karl Chen <quarl@cs.berkeley.edu>
2006-06-16 04:22:42 +0000

    Err-match fix

--- elsa/in/k0076.cc
+++ elsa/in/k0076.cc
@@ -4,7 +4,7 @@
 
 // Assertion failed: e != NULL && "388cba6d-895c-4acb-ac25-c28fc1c4c2cb", file cc.gr line 1234
 
-// ERR-MATCH: Assertion failed: 388cba6d-895c-4acb-ac25-c28fc1c4c2cb
+// ERR-MATCH: 388cba6d-895c-4acb-ac25-c28fc1c4c2cb
 
 struct S1 {};
 int foo(struct S1 __attribute__((unused)) *x)
