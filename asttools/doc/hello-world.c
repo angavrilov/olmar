c7924de757a6f1bb5a70aabbf6ad232ea54ff626
Hendrik Tews <tews@cs.ru.nl>
2006-09-04 23:08:52 +0000

    * picture hackery

---
+++ asttools/doc/hello-world.c
@@ -0,0 +1,10 @@
+extern int printf (__const char *__restrict __format, ...);
+
+int main(){
+  printf("Hello ocaml world\n");
+  return 0;
+}
+
+/// Local Variables: ///
+/// compile-command: "gcc -Wall hello-world.c" ///
+/// End: ///
