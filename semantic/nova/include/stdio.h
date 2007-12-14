e96b2a1621118cdf8e53b5b88c36c5ae75c18ca2
Marcus Völp <voelp@os.inf.tu-dresden.de>
2007-12-14 08:04:54 +0000

    - new stdio.h to disable all debugging

--- semantic/nova/include/stdio.h
+++ semantic/nova/include/stdio.h
@@ -1,43 +1,16 @@
-/*
- * \file    $Id$
- * \brief   Standard I/O
- * \author  Udo Steinberg <udo@hypervisor.org>
+/* Marcus 
  *
- * Copyright (C) 2005-2007, Udo Steinberg <udo@hypervisor.org>
- * Technische Universitaet Dresden, Operating Systems Group
- * All rights reserved.
+ * This file replaces nova/include/stdio.h
+ *
+ * all debug functions need to be declared as empty macros
  */
-
 #pragma once
 
-#include "compiler.h"
-#ifndef SEMANTICS_COMPILER
-#include "console_serial.h"
-#include "console_vga.h"
-#include "cpu.h"
-#include "memory.h"
-#include "stdarg.h"
-#endif
+#define trace(T, format, ...)
+#define panic(format, ...)
+#define printf(format, ...)
+#define vprintf(format, args)
 
-#define trace(T,format,...)                                                     \
-do {                                                                            \
-    register mword __esp asm ("esp");                                           \
-    if (EXPECT_FALSE ((trace_mask & T) == T))                                   \
-        printf ("[%d] " format "\n",                                            \
-               ((__esp - 1) & ~0xfff) == KSTCK_SADDR ?                          \
-                Cpu::id : ~0u, ## __VA_ARGS__);                                 \
-} while (0)
-
-#define NOOUTPUT
-#ifdef NOOUTPUT
-#undef trace
-#define trace(T,format,...)
-#endif
-
-#ifndef SEMANTICS_COMPILER
-/*
- * Definition of trace events
- */
 enum {
     TRACE_CPU       = 1ul << 0,
     TRACE_VMX       = 1ul << 1,
@@ -57,43 +30,4 @@ enum {
     TRACE_CONTEXT   = 1ul << 15
 };
 
-/*
- * Enabled trace events
- */
-unsigned const trace_mask =
-                            TRACE_CPU       |
-#ifndef NDEBUG
-                            TRACE_VMX       |
-                            TRACE_SMX       |
-                            TRACE_APIC      |
-//                            TRACE_MEMORY    |
-//                            TRACE_PTAB      |
-                            TRACE_TIMER     |
-//                            TRACE_INTERRUPT |
-                            TRACE_ACPI      |
-                            TRACE_KEYB      |
-                            TRACE_EC        |
-                            TRACE_PD        |
-                            TRACE_SYSCALL   |
-                            TRACE_DMAR      |
-//                            TRACE_PCI       |
-//                            TRACE_CONTEXT   |
-#endif
-                            0;
-
-unsigned init_spinner (unsigned cpu);
-
-FORMAT (1,0)
-void vprintf (char const *format, va_list args);
-
-FORMAT (1,2)
-void printf (char const *format, ...);
-
-FORMAT (1,2) NORETURN
-void panic (char const *format, ...);
-
-extern Console_serial serial;
-extern Console_vga    screen;
-#else
-void panic (char const *format, ...) {};
-#endif
+unsigned const trace_mask = 0;
